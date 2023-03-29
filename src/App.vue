b1f9f889294ee5bb24e99c49c1a32d4a766ae31e
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-29 14:20:29 +0200

    installazione Axios per chiamate API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <!-- PARTE VUE JS-->
 <script>
-import { store } from './components/store.js'
+import axios from 'axios'; //richiamo import axios
+import { store } from './components/store.js' //richiamo collegamento store.js
+
 import DefaultComponent from './components/DefaultComponent.vue';
 
 export default {
