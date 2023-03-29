4de95e679b13d2ea2e298b115ce7f155f463e840
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-29 14:17:12 +0200

    predisposizione store.js

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <!-- PARTE VUE JS-->
 <script>
+import { store } from './components/store.js'
 import DefaultComponent from './components/DefaultComponent.vue';
 
 export default {
   name: 'App',
   components: {
     DefaultComponent
+  },
+  data() {
+    return {
+      store
+    }
   }
 }
 
